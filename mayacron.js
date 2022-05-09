7b395c073997b7fb2d6e7ae05b32b3fd76ed5fef
cje <IngaKatten@Administrators-MacBook-Pro.local>
2022-05-09 15:11:40 -0500

    Added documentation to mayacron.js

--- mayacron.js
+++ mayacron.js
@@ -2,6 +2,11 @@
 
     function getMayan(yyyy, mm, dd) {
 
+      // Note: The value associated with the energy in this enum is only for calculation purposes.
+      // The Number associated with each energy changes 5 positions from year to year due to the uayeb (5 extra days). 
+      // For example:
+      // On 2018.04.13, the energy was CABAN and it fell on day 11 of the 20 day cycle.
+      // On 2018.03.03, the energy was CABAN and it fell on day 16 of the 20 day cycle.
        var MayanEnergyEnum = {
          1 : "IK",
          2 : "AKBAL",
